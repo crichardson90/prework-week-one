56a88442c1913fe0078d9eaf5861f1e8a176953f
Coty Richardson <crichardson90@live.com>
2018-09-19 17:51:53 -0500

    fullname.rb

---
+++ fullname.rb
@@ -0,0 +1,9 @@
+puts "Hello what is your first name?"
+
+first_name = gets.chomp
+
+puts "What is your last name?"
+
+last_name = gets.chomp
+
+puts "Hello " + first_name  + " " + last_name + ", how are you?"
